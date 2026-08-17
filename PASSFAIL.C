a4c7a4108c8fcf7de99e7483815e3e2bdcff2448
Tanmaylachhwani <tanmaylachhwani80@gmail.com>
2026-08-17 11:33:04 +0530

    Add files via upload

---
+++ PASSFAIL.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int mark;
+printf("Enter your mark");
+scanf("%d",&mark);
+if(mark >=33 )
+printf("pass");
+else
+printf("fail");
+printf("\n\tthank you");
+getch();
+}
+
+
+
+
